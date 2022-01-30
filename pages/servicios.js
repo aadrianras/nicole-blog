9fde8ad851c1649a12f455e9d3c9dac7c73a971e
Adrian Aguilar <aadrianras@gmail.com>
2022-01-30 19:20:50 -0400

    Services page: We added new services and changed pictures

--- pages/servicios.js
+++ pages/servicios.js
@@ -2,7 +2,7 @@
 import ContactBox from '../components/contact/ContactBox';
 import Testimonials from '../components/testimonials/Testimonials';
 
-import { hero, hero_text, common, title, masotherapy, description, kinesitherapy, electrotherapy, respiratory, division } from './servicios.module.css';
+import { hero, hero_text, common, title, masotherapy, description, kinesitherapy, electrotherapy, respiratory, lymphatic, geriatric, neurological, treatment, division } from './servicios.module.css';
 
 
 const servicios = ({ data: { clientsOpinion } }) => {
@@ -22,28 +22,46 @@ const servicios = ({ data: { clientsOpinion } }) => {
             </div>
 
 
-            <div className={`${masotherapy}`}>
-                <h2 className={title}>Masoterápia</h2>
+            <div className={`${masotherapy} ${treatment}`}>
+                <h2 className={title}>Fisioterapia manual</h2>
             </div>
-            <p className={description}>Sit officia enim velit dolor culpa et. Eu anim commodo occaecat pariatur irure ipsum ea eiusmod reprehenderit excepteur do ullamco qui. Est esse in eu sunt labore consectetur laboris ipsum ex id labore ipsum commodo esse. Voluptate consectetur commodo fugiat ut laborum sit in culpa dolore ex laboris.</p>
+            <p className={description}><b>La Fisioterapia Manual</b> se basa en técnicas específicas para el tratamiento del dolor y otros sintomas de disfunción neuro-músculo-esquelética en la columna vertebral y/o extremidades. Estas técnicas son aplicadas manualmente basándose en <b>movilizaciones, manipulaciones, masajes terapéuticos, masajes relajantes y/o masajes para estimular el riego sanguíneo.</b></p>
 
 
-            <div className={`${kinesitherapy}`}>
+            <div className={`${kinesitherapy} ${treatment}`}>
                 <h2 className={title}>Cinesiterapia</h2>
             </div>
-            <p className={description}>Laboris in mollit eu duis commodo duis sit magna magna. Esse dolore adipisicing enim qui incididunt incididunt mollit voluptate ea. Pariatur velit culpa pariatur qui ex ex proident ipsum mollit aute sit esse adipisicing. Incididunt consequat sint quis incididunt eu eu. Minim fugiat mollit occaecat mollit anim reprehenderit proident tempor.</p>
+            <p className={description}><b>La Cinesiterapia</b> se aplica mediante técnicas y procedimientos basados en el movimiento cinético, cinemático y dinámico trato distintas patologías o alteraciones músculo-esqueléticas, mayormente encontradas en el adulto mayor.</p>
 
 
-            <div className={`${electrotherapy}`}>
+            <div className={`${electrotherapy} ${treatment}`}>
                 <h2 className={title}>Electroterapia</h2>
             </div>
-            <p className={description}>Excepteur exercitation ut fugiat ullamco duis adipisicing reprehenderit tempor. Cupidatat irure amet aliqua laboris est voluptate Lorem laboris in proident voluptate. Aliqua nisi proident elit nisi consectetur sint. Magna dolore elit nulla nisi laborum id quis dolore culpa. Lorem minim id duis sunt aliqua nulla ex eiusmod ut cillum esse. In tempor eiusmod elit commodo elit labore aliqua laborum quis proident incididunt eu reprehenderit. Ea consectetur ea commodo dolor sint adipisicing.</p>
+            <p className={description}><b>La Electroterapia</b> es la técnica comunmente utilizada para la rehabilitación terapéutica, es decir, mediante la aplicación profesional de corriente eléctrica se estimula el fortalecimiento de la masa muscular, articular u otros. Asimismo, se utiliza como un medio de recuperación en todo tipo de afecciones.</p>
 
 
-            <div className={`${respiratory}`}>
+            <div className={`${respiratory} ${treatment}`}>
                 <h2 className={title}>Terapia Respiratoria</h2>
             </div>
-            <p className={description}>Fugiat magna veniam commodo enim ea fugiat officia nisi nulla cillum. Duis aliqua proident aliquip aute ipsum nulla quis enim reprehenderit esse laboris. Exercitation proident esse anim eiusmod sit voluptate dolor commodo dolore. Nostrud ad laboris velit reprehenderit ex do reprehenderit ex mollit aliquip velit mollit. Veniam dolore reprehenderit incididunt anim nostrud voluptate ullamco est est excepteur. Ipsum consectetur dolore nulla sint fugiat tempor minim do. Lorem mollit minim proident officia excepteur pariatur ad laboris nisi tempor nostrud Lorem consectetur.</p>
+            <p className={description}><b>La Terapia Respiratoria</b> se basa en el conjunto de técnicas que tratan las diferentes patologías cardio-respiratorias. </p>
+
+
+            <div className={`${lymphatic} ${treatment}`}>
+                <h2 className={title}>Drenaje Linfático</h2>
+            </div>
+            <p className={description}><b>El Drenaje Linfático</b> se basa en un método de masajes específicos con maniobras precisas, suaves, superficiales y ritmicas. Esté tratamiento se aplica sobre el sistema cirtulatorio y su objetivo es movilizar los liquidos del organismo para favorecer la eliminación de sustancias de desecho que se acumulan en la sangre. Este tratamiento se aplica en edemas localizados y generalizados, celulítis, problemas de cirulación, piernas cansadas, procesos de cicatrización, etc...</p>
+
+
+            <div className={`${geriatric} ${treatment}`}>
+                <h2 className={title}>Terapia Geriátrica</h2>
+            </div>
+            <p className={description}><b>La Terapia Geriátrica</b> ofrece una alternativa no farmacológica preventiva y de mantenimiento, especialmente diseñada para personas mayores de 65 años, con el objetivo de mejorar la capacidad fisica del adulto mayor, evitando el deterioro progresivo y mejorando su calidad de vida.</p>
+
+
+            <div className={`${neurological} ${treatment}`}>
+                <h2 className={title}>Terapia Neurológica</h2>
+            </div>
+            <p className={description}><b>La Terapia Neurológica</b> se dedica al tratamiento de lesiones ocasionadas por la afección del sistema nervioso. El objetivo es mejorar las capacidades fisicas y alteraciones estructurales del paciente neurológico, para así poder mejorar la calidad y efectividad del movimiento en cuanto a su postura.</p>
 
 
             <ContactBox />
