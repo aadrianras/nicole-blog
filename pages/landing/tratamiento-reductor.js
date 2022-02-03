997d50d5938f706ff7945a5de3ec05634b76251a
Adrian Aguilar <aadrianras@gmail.com>
2022-02-03 13:56:42 -0400

    Weight reduction landing page: Google map width responsive corrected

--- pages/landing/tratamiento-reductor.js
+++ pages/landing/tratamiento-reductor.js
@@ -205,7 +205,7 @@ const ReductorTreatment = () => {
                             </a>
                         </div>
                         <div className={map_c}>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d593.7129280853162!2d-68.07746273475776!3d-16.54248475567715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f21131fdcbe63%3A0x7efd00803d883f20!2sEdif.%20Monroy%20y%20velez!5e0!3m2!1sen!2sbo!4v1643825458108!5m2!1sen!2sbo" width="400" height="300" style={{ border: 0 }} allowFullScreen={false} loading="lazy"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d593.7129280853162!2d-68.07746273475776!3d-16.54248475567715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x915f21131fdcbe63%3A0x7efd00803d883f20!2sEdif.%20Monroy%20y%20velez!5e0!3m2!1sen!2sbo!4v1643825458108!5m2!1sen!2sbo" width="100%" height="300" style={{ border: 0 }} allowFullScreen={false} loading="lazy"></iframe>
                         </div>
                     </div>
                 </div>
