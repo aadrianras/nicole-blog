7b91d9f0f9c60a4654cd39dff6e60a0f354a806f
Adrian Aguilar <aadrianras@gmail.com>
2022-02-08 16:42:00 -0400

    Fix: Google AdSense added to the head tag of the _document file

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=IM+Fell+Double+Pica&family=Montserrat:wght@300;400&display=swap" rel="stylesheet" />
+                    <script data-ad-client="ca-pub-3457873907604920" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                 </Head >
                 <body>
                     <Main />
